a1a3907806c29464de22e1e90bcd8c98478315ee
nikunj goyal <nikunjgoyal31@gmail.com>
2021-10-14 16:12:51 +0530

    added game function

--- js/script.js
+++ js/script.js
@@ -58,3 +58,29 @@ levels[2] = {
       y:4
   }
  };
+
+ function Game(id,level) {
+  
+    this.el = document.getElementById(id);
+    
+    // level addition
+    this.level_idx = 0;
+    
+    // establish the basic properties common to all this objects.
+    this.tileTypes = ['floor','wall'];
+    this.tileDim = 32;
+    // inherit the level's properties: map, player start, goal start.
+    this.map = level.map;
+    
+    // level switch
+    this.theme = level.theme;
+    
+    // make a copy of the level's player.
+    this.player = {...level.player};
+    
+    // create a property for the DOM element, to be set later.
+    this.player.el = null;
+    
+    // make a copy of the goal.
+    this.goal = {...level.goal};
+  }
